6d619c438d3a679d576d67084b074cbbc775c4e8
Madhan-vj <59563681+Madhan-vj@users.noreply.github.com>
2020-05-09 14:28:03 +0530

    api

--- src/app/topstory/topstory.component.ts
+++ src/app/topstory/topstory.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from "../news.service";
 import { HttpClient, HttpParams } from "@angular/common/http";
-
+import { news } from "./api";
 @Component({
   selector: 'app-topstory',
   templateUrl: './topstory.component.html',
@@ -9,11 +9,12 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 })
 export class TopstoryComponent implements OnInit {
   articles;
+  results = [];
   constructor(private service: NewsService ,
     private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.service.getData().subscribe((data)=>{
+    this.service.getData().subscribe((data: news)=>{
       this.articles = data.results;
       console.log(data.results);
       // console.log(data.results[0].multimedia[4].url);
